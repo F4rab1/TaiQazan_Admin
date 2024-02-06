80daffad0b91244381531edc92d460517686f255
F4rab1 <issafarabi2022@gmail.com>
2024-02-07 01:29:14 +0600

    product Image

--- TaiQazan_Admin/ViewController.swift
+++ TaiQazan_Admin/ViewController.swift
@@ -6,14 +6,55 @@
 //
 
 import UIKit
+import SnapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    private let productImageButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "plus_photo"), for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.navigationItem.title = "Add product"
+        setupUI()
+        setupConstraints()
     }
-
+    
+    private func setupUI() {
+        view.backgroundColor = .white
+        productImageButton.addTarget(self, action: #selector(handlePlusPhoto), for: .touchUpInside)
+        
+        [productImageButton].forEach {
+            view.addSubview($0)
+        }
+    }
+    
+    private func setupConstraints() {
+        productImageButton.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(200)
+        }
+    }
+    
+    @objc func handlePlusPhoto() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let originalImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            productImageButton.setImage(originalImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
 
 }
 
