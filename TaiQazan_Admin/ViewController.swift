f8b64d81b875ce4cdaaa582b1229ce6880c4c477
F4rab1 <issafarabi2022@gmail.com>
2024-02-06 23:29:03 +0600

    Initial Commit

---
+++ TaiQazan_Admin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaiQazan_Admin
+//
+//  Created by Фараби Иса on 06.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
